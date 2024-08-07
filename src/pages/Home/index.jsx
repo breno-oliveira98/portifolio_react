ea74fd24b5bb13ca6bf7f62aec4f8cc96d68171c
Breno Oliveira <breno.oliveira2011@hotmail.com>
2024-08-06 23:18:22 -0300

    ajustes no css

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,28 @@
-import Container from '../../components/Container'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 import styles from './Home.module.css'
+import { Link } from 'react-router-dom'
 
 function Home() {
     return (
         <>
         <Header />
-        <Container/>
+        <section className={styles.home}>
+        <div className='apresentacao'>
+          <p>
+            Olá, sou <br />
+            <span>Breno Oliveira</span> <br />
+            Dev Full Stack
+          </p>
+          <Link to="/sobre" className='btn'>
+            Saiba mais sobre mim
+          </Link>
+        </div>
+        <figure>
+            <img className='img-home' src="/dev-img.svg" alt="Imagem da Home" />
+        </figure>
+
+      </section>
         <Footer/>
       </>
     )
